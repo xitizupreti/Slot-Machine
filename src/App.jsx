bb5b094ff5242c53401770f3efbe8fc52ce3fa3a
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-06-06 19:37:28 +0545

    Slot Machine Game

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+// function ref(){
+    
+// }
+
+const SlotM = () => {
+    // let x='😁';
+    // let y='😁';
+    // let z='😁';
+    let icons=["❤️","💔"]
+    let x=icons[Math.floor(Math.random()*icons.length)];
+    let y=icons[Math.floor(Math.random()*icons.length)];;
+    let z=icons[Math.floor(Math.random()*icons.length)];;
+
+    if((x===y) && (y===z)){
+        return(
+            <div className="slot_inner">
+                <h1> {x} {y} {z}</h1>
+                <h1>This is matching</h1>
+                <hr />
+            </div>
+        )
+    }
+    else{
+        return(
+            <div className="slot_inner">
+                <h1> {x} {y} {z}</h1>
+                <h1>This is not matching</h1>
+                <hr />
+            </div>
+        )
+    }
+}
+
+const App = () => {
+    return (
+        <>
+        <h1 className="heading_style">
+            🎰 Welcome to <span style={{fontWeight:"bold"}}>Slot Machine Game</span> 🎰
+        </h1>
+        <div className="slotmachine">
+        {/* <SlotM x="😁" y="😁" z="😁" />
+        <SlotM x="😁" y="🤣" z="😁" />
+        <SlotM x="❤️" y="💘" z="❤️" />
+        <SlotM x="😭" y="😭" z="😭" /> */}
+        <SlotM />
+        <SlotM />
+        <SlotM />
+        <br />
+        <button type="button" onClick="window.location.reload();">Play Again!</button>
+
+        </div>
+        </>
+    )
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./Cards";
+import App from "./App";
 import "./index.css";
-import data from "./Data.jsx";
-
-function nf(val) {
-  return (
-    <Card
-    key={val.id}
-      imgsrc={val.imgsrc}
-      title={val.title}
-      sname={val.sname}
-      link={val.imgsrc}
-    />
-  );
-}
 
 ReactDOM.render(
   <>
-    <h1 className="heading_style">List of my Best Netflix Series Till Now!</h1>
-    {data.map(nf)};
-    <footer></footer>
+  < App />
   </>,
   document.getElementById("root")
-);
+)
